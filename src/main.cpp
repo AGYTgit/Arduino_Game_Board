8e895a9f6acad6d997ab141fe18bb490b00c0945
AGYTgit <bubonb12@gmail.com>
2024-06-22 16:26:16 +0200

    button grid scanning (working)

--- src/main.cpp
+++ src/main.cpp
@@ -11,34 +11,54 @@
 
 // Block block = {5, 3, 0, 0};
 
+#define BUTTONS_WIDTH 2
+#define BUTTONS_HEIGHT 2
+
+#define INPUT_BUTTON_OFFSET 8
+#define OUTPUT_BUTTON_OFFSET 10
+
+bool test[BUTTONS_HEIGHT][BUTTONS_WIDTH];
+
 void setup() {
   Serial.begin(9600);
 
   // lcd.Init();
   // lcd.fill();
 
-  pinMode(7, INPUT_PULLUP);
-  digitalWrite(7, HIGH);
-  
-  pinMode(8, OUTPUT);
-
   // board.add_block(block);
   // board.draw(lcd);
+
+  for (int ib = 0; ib < BUTTONS_WIDTH; ib++) {
+    pinMode(ib + INPUT_BUTTON_OFFSET, INPUT_PULLUP);
+    for (int jb = 0; jb < BUTTONS_HEIGHT; jb++) {
+      pinMode(jb + OUTPUT_BUTTON_OFFSET, OUTPUT);
+      test[jb][ib] = true;
+    }
+  }
 }
 
-// bool test[3][3] = {
-//   {true,true,true},
-//   {true,true,true},
-//   {true,true,true}
-// };
-
 void loop() {
-  // if (digitalRead(7) != 0 && test[0]) {
-  //   test[0][0] = false;
-  //   Serial.println(0);
-  // } else if (digitalRead(7) == 0 && !test[0]) {
-  //   test[0][0] = true;
-  //   Serial.println(1);
-  // }
-  Serial.println(digitalRead(7));
+  for (int i = 0; i < BUTTONS_WIDTH; i++) {
+    digitalWrite(i + INPUT_BUTTON_OFFSET, HIGH);
+    for (int ip = 1; ip < BUTTONS_WIDTH; ip++) {
+      digitalWrite(((i + ip) % BUTTONS_WIDTH) + INPUT_BUTTON_OFFSET, LOW);
+    }
+
+    for (int j = 0; j < BUTTONS_HEIGHT; j++) {
+      digitalWrite(j + OUTPUT_BUTTON_OFFSET, LOW);
+      for (int jp = 1; jp < BUTTONS_HEIGHT; jp++) {
+        digitalWrite(((j + jp) % BUTTONS_HEIGHT) + OUTPUT_BUTTON_OFFSET, HIGH);
+      }
+      
+      if (digitalRead(i + INPUT_BUTTON_OFFSET) == LOW && test[j][i]) {
+        test[j][i] = false;
+      } else if (digitalRead(i + INPUT_BUTTON_OFFSET) != LOW && !test[j][i]) {
+        test[j][i] = true;
+        Serial.print(j);
+        Serial.print(' ');
+        Serial.println(i);
+      }
+      delayMicroseconds(30);
+    }
+  }
 }
